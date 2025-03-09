68e0993ddc24b807628fff353dece424db699768
Manjay Kumar <103574933+manjay6428@users.noreply.github.com>
2025-03-09 09:13:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5002;
 
 app.use(
   cors({
-    origin: "https://mern-e-commerce-1-9nqg.onrender.com/",
+    origin: "https://mern-e-commerce-1-9nqg.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
       "Content-Type",
